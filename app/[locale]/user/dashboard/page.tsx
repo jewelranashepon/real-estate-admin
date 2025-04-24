c9503be6598a86e00d9eb9202d735c1e03847df5
faysalmohammed2020 <faysalmohammed.shah@gmail.com>
2025-04-24 07:13:14 +0600

    updated locale userboard

--- app/[locale]/user/dashboard/page.tsx
+++ app/[locale]/user/dashboard/page.tsx
@@ -151,19 +151,19 @@ export default function Dashboard() {
               value="saved"
               className="data-[state=active]:bg-gradient-to-r data-[state=active]:from-emerald-500/20 data-[state=active]:to-green-500/20 data-[state=active]:border data-[state=active]:border-emerald-500/30"
             >
-              Saved Properties
+             {t('common.SP')}
             </TabsTrigger>
             <TabsTrigger
               value="recent"
               className="data-[state=active]:bg-gradient-to-r data-[state=active]:from-emerald-500/20 data-[state=active]:to-green-500/20 data-[state=active]:border data-[state=active]:border-emerald-500/30"
             >
-              Recent Searches
+              {t('common.RS')}
             </TabsTrigger>
             <TabsTrigger
               value="messages"
               className="data-[state=active]:bg-gradient-to-r data-[state=active]:from-emerald-500/20 data-[state=active]:to-green-500/20 data-[state=active]:border data-[state=active]:border-emerald-500/30"
             >
-              Messages
+             {t('common.Messages')}
             </TabsTrigger>
           </TabsList>
 
@@ -174,7 +174,7 @@ export default function Dashboard() {
                 <>
                   <div className="col-span-full flex justify-between items-center mb-4">
                     <h2 className="text-xl font-semibold">
-                      Your Saved Properties
+                    {t('common.YSP')}
                     </h2>
                     <Button
                       variant="outline"
@@ -183,7 +183,7 @@ export default function Dashboard() {
                       className="border-emerald-800/30 bg-emerald-950/10"
                     >
                       <Heart className="h-4 w-4 mr-2" />
-                      View All Saved
+                      {t('common.VAS')}
                     </Button>
                   </div>
                   {properties
@@ -447,7 +447,7 @@ export default function Dashboard() {
         </Tabs>
 
         <div className="mb-8">
-          <h2 className="text-xl font-semibold mb-4">Featured Properties</h2>
+          <h2 className="text-xl font-semibold mb-4">{t('common.FP')}</h2>
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
             {properties
               .filter((property) => property.isFeatured)
