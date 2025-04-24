51d5782d849c8333efa28237e414758a2c4e7ee6
estiak98 <estiakahmed898@gmail.com>
2025-04-24 07:34:19 +0600

    Service and Contact added

--- components/landingpage/navbar.tsx
+++ components/landingpage/navbar.tsx
@@ -12,10 +12,7 @@ export default function Navbar() {
 
   const navLinks = [
     { name: t("nav.home"), href: "/" },
-    { name: t("nav.properties"), href: "/properties" },
-    { name: t("nav.agents"), href: "/agents" },
-    { name: t("nav.service"), href: "/services" },
-    { name: t("nav.about"), href: "/about" },
+    { name: t("nav.service"), href: "/service" },
     { name: t("nav.contact"), href: "/contact" },
   ];
 
