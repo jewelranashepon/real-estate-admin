e6c29a91e8808a0dfdf4d7544211654ba4588b5a
tauhid97k <tauhid.coder@gmail.com>
2025-04-26 15:49:38 +0600

    update fonts based on locale

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,50 @@
 import type { Metadata } from "next";
+import { Inter, Amiri } from "next/font/google";
 import { Toaster } from "../components/ui/sonner";
-import "./globals.css";
 import "@emran-alhaddad/saudi-riyal-font/index.css";
+import "./globals.css";
 import { SearchProvider } from "@/lib/search-context";
+import { cn } from "@/lib/utils";
+import { getLocale } from "next-intl/server";
 
 export const metadata: Metadata = {
   title: "real-estate-app",
   description: "Real Estate",
 };
 
-export default function RootLayout({
+// English
+const inter = Inter({
+  subsets: ["latin"],
+  display: "swap",
+});
+
+// Arabic
+const amiri = Amiri({
+  weight: ["400", "700"],
+  subsets: ["arabic"],
+  display: "swap",
+});
+
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const locale = await getLocale();
+
   return (
-    <html suppressHydrationWarning>
-      <body style={{ fontFamily: "saudi_riyal" }}>
+    <html
+      lang={locale}
+      className={`${inter.className} antialiased`}
+      suppressHydrationWarning
+    >
+      <body
+        className={cn("antialiased", {
+          [inter.className]: locale === "en",
+          [amiri.className]: locale === "ar",
+        })}
+        style={{ fontFamily: "saudi_riyal" }}
+      >
         <SearchProvider>{children}</SearchProvider>
         <Toaster />
       </body>
