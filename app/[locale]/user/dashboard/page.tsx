d61dc81335fe8c48b68c55aefc8491dfe8cc841c
tauhid97k <tauhid.coder@gmail.com>
2025-04-26 18:10:52 +0600

    update user dashboard

--- app/[locale]/user/dashboard/page.tsx
+++ app/[locale]/user/dashboard/page.tsx
@@ -32,7 +32,6 @@ import { useTranslations } from "next-intl";
 export default function Dashboard() {
   const router = useRouter();
   const t = useTranslations();
-  const t = useTranslations();
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [isSearchModalOpen, setIsSearchModalOpen] = useState(false);
   const [selectedProperty, setSelectedProperty] = useState<string | null>(null);
